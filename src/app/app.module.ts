2257104fe7e139941dfe27dc761ee0475e1a7213
FernandoLD99 <fernojo99@gmail.com>
2023-08-23 20:49:28 -0600

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations:[  
+        AppComponent, 
+        LoginComponent, 
+        NoPageFoundComponent, 
+        DashboardComponent, 
+        BreadcrumbsComponent, 
+        SidebarComponent,
+    ],
+    imports:[
+        BrowserModule
+    ],
+    providers:[],
+
+    bootstrap:[AppComponent]
 })
-export class AppModule { }
+
+export class AppModule{ }
